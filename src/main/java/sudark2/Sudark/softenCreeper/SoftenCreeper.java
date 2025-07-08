d466f37932fea8e3fc835a73b2ab12093e10cd28
SUDARK <2963502563@qq.com>
2025-07-08 21:27:45 +0800

    Sudark

--- src/main/java/sudark2/Sudark/softenCreeper/SoftenCreeper.java
+++ src/main/java/sudark2/Sudark/softenCreeper/SoftenCreeper.java
@@ -5,10 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
-import org.bukkit.entity.Creeper;
-import org.bukkit.entity.EnderCrystal;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.WitherSkull;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
@@ -29,11 +26,9 @@ public final class SoftenCreeper extends JavaPlugin {
         @EventHandler
         public void onExplode(EntityExplodeEvent e) {
             Entity en = e.getEntity();
-            if (en instanceof Creeper || en instanceof EnderCrystal || en instanceof WitherSkull) {
+            if (en instanceof Creeper || en instanceof EnderCrystal || en instanceof WitherSkull || en instanceof Wither) {
                 // 复制方块原始状态
-                List<BlockState> blocks = e.blockList().stream()
-                        .map(block -> block.getState())
-                        .collect(Collectors.toList());
+                List<BlockState> blocks = e.blockList().stream().map(block -> block.getState()).collect(Collectors.toList());
 
                 // 移除所有方块，避免掉落物
                 for (Block block : e.blockList()) {
@@ -46,11 +41,7 @@ public final class SoftenCreeper extends JavaPlugin {
                     @Override
                     public void run() {
                         for (BlockState block : blocks) {
-                            block.getLocation().getWorld().spawnParticle(
-                                    Particle.SCULK_SOUL,
-                                    block.getLocation(),
-                                    2, 0.1f, 0.1f, 0.1f, 0.2f
-                            );
+                            block.getLocation().getWorld().spawnParticle(Particle.SCULK_SOUL, block.getLocation(), 2, 0.1f, 0.1f, 0.1f, 0.2f);
                             block.update(true, false); // true=force, false=don't apply physics
                         }
                     }
