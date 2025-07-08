9332419c2a3a5e00efe444b7a9bf770f6d34854d
SUDARK <2963502563@qq.com>
2025-07-08 12:57:03 +0800

    Initial commit

---
+++ src/main/java/sudark2/Sudark/softenCreeper/SoftenCreeper.java
@@ -0,0 +1,16 @@
+package sudark2.Sudark.softenCreeper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SoftenCreeper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukk
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
